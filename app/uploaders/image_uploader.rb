c2d3078647d11381fdb07bc789ae069d5b3ea424
Chris <chris.lam.unt@gmail.com>
2016-10-20 22:09:06 -0500

    styled index a bit to create responsive images

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,15 +2,15 @@ class ImageUploader < CarrierWave::Uploader::Base
 
  include CarrierWave::RMagick
 
-  #storage :file
-  storage :fog
+  storage :file
+  #storage :fog
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
     version :thumb do
-    process :resize_to_limit => [200, 200]
+    process :resize_to_limit => [400, 400]
   end
 
 end
