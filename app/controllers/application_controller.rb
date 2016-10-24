f8e0aa7e3a3e1af96b4d7127d20ee58469da77ab
Chris <chris.lam.unt@gmail.com>
2016-10-24 14:11:47 -0500

    dependent destroy added to claims and journeys

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,4 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
 	  journeys_path
 	end
-
-	
 end
--- app/models/claim.rb
+++ app/models/claim.rb
@@ -1,7 +1,7 @@
 class Claim < ActiveRecord::Base
 	validates :answer, presence: true
 	
-	belongs_to :journey
+	belongs_to :journey, :dependent => :destroy
 	belongs_to :explorer, class_name: "User"
 	belongs_to :creator, class_name: "User"
 
