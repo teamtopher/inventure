90216bec89ef7ae493ebed6055c2f4f81c36686b
Chris <chris.lam.unt@gmail.com>
2016-11-02 12:06:41 -0500

    removed rmagick for now

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,6 +1,6 @@
 class ImageUploader < CarrierWave::Uploader::Base
 
-  include CarrierWave::RMagick
+  include CarrierWave
 
   #storage :file
   storage :fog
@@ -9,8 +9,5 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-    version :thumb do
-    process :resize_to_limit => [400, 400]
-  end
 
 end
