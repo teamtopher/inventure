49cba0f6942d92c046ff0e4a0d2a52890e02e1d0
Chris <chris.lam.unt@gmail.com>
2016-11-18 14:57:55 -0600

    added username and avatar image to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161117212733) do
+ActiveRecord::Schema.define(version: 20161118204355) do
 
   create_table "claims", force: :cascade do |t|
     t.string   "answer"
@@ -62,6 +62,8 @@ ActiveRecord::Schema.define(version: 20161117212733) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.integer  "journey_id"
+    t.text     "username"
+    t.string   "image"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
