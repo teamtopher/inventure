459c078a96e3b6298fad9d55db5981fa11011f8b
Chris <chris.lam.unt@gmail.com>
2016-11-21 14:16:56 -0600

    redirect after updating profeil

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,13 +4,14 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception  
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-
-  def after_sign_in_path_for(resource)
-	  pages_choose_path
-	end
-  def configure_permitted_parameters
+def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
     devise_parameter_sanitizer.permit(:account_update, keys: [:username])
 
   end
+  def after_sign_in_path_for(resource)
+	  pages_choose_path
+	end
+  
+  
 end
